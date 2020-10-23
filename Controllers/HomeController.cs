b2db68e73b2320225743ab055cc312a360233ed5
Jeffrey Dale Starr <j.starr@wearedoubleline.com>
2020-10-23 11:15:04 -0500

    initial upload of project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using UpBell.Models.UpBell;
+
+namespace UpBell.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ProcessSchedules()
+        {
+            var response = new UpBellResponse();
+
+            var f = Request.Files[0];
+
+            var fileType = f.FileName.Substring(f.FileName.LastIndexOf('.') + 1).ToLower();
+
+            if (fileType == "csv")
+            {
+                using (var reader = new StreamReader(f.InputStream))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(',');
+
+                        var s = new UpBellSchedule
+                        {
+                            Summary = values[0],
+                            Location = values[1],
+                            Description = values[2],
+                            Date = values[3],
+                            StartTime = values[4],
+                            EndTime = values[5],
+                            FullStartDateTime = $"{values[3]}T{values[4]}",
+                            FullEndDateTime = $"{values[3]}T{values[5]}"
+                        };
+
+                        if (s.Summary != "Summary")
+                        {
+                            response.Schedules.Add(s);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                string json;
+
+                using (StreamReader reader = new StreamReader(f.InputStream))
+                {
+                    json = reader.ReadToEnd();
+                }
+                response.Schedules = JsonConvert.DeserializeObject<List<UpBellSchedule>>(json);
+
+                foreach (var s in response.Schedules)
+                {
+                    s.FullStartDateTime = $"{s.Date}T{s.StartTime}";
+                    s.FullEndDateTime = $"{s.Date}T{s.EndTime}";
+                }
+            }
+
+            var lstEvent = new List<UpBellEvent>();
+
+            foreach (var s in response.Schedules)
+            {
+                var e = new UpBellEvent();
+                e.description = s.Description;
+                e.location = s.Location;
+                e.start.dateTime = s.FullStartDateTime;
+                e.start.timeZone = "America/Chicago";
+                e.end.dateTime = s.FullEndDateTime;
+                e.end.timeZone = "America/Chicago";
+                e.summary = s.Summary;
+                lstEvent.Add(e);
+            }
+
+            response.Message = "Success";
+            response.Events = lstEvent;
+
+            return Json(response); ;
+        }
+    }
+}
---
+++ Models/UpBell/UpBellModels.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UpBell.Models.UpBell
+{
+    public class UpBellSchedule
+    {
+        public string Summary { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string FullStartDateTime { get; set; }
+        public string FullEndDateTime { get; set; }
+    }
+
+    public class UpBellResponse
+    {
+        public string Message { get; set; }
+        public List<UpBellSchedule> Schedules { get; set; }
+        public List<UpBellEvent> Events { get; set; }
+
+        public UpBellResponse()
+        {
+            Schedules = new List<UpBellSchedule>();
+            Events = new List<UpBellEvent>();
+        }
+    }
+
+    public class UpBellEvent
+    {
+        public string summary { get; set; }
+        public string location { get; set; }
+        public string description { get; set; }
+        public UpBellEventDate start { get; set; }
+        public UpBellEventDate end { get; set; }
+        public List<string> recurrence { get; set; }
+        public UpBellReminder reminders { get; set; }
+
+        public UpBellEvent()
+        {
+            recurrence = new List<string> { "RRULE:FREQ=DAILY;COUNT=1" };
+            reminders = new UpBellReminder();
+            start = new UpBellEventDate();
+            end = new UpBellEventDate();
+        }
+    }
+
+    public class UpBellEventDate
+    {
+        public string dateTime { get; set; }
+        public string timeZone { get; set; }
+    }
+
+    public class UpBellReminder
+    {
+        public bool useDefault { get; set; }
+        public List<UpBellReminderOverride> overrides { get; set; }
+
+        public UpBellReminder()
+        {
+            useDefault = false;
+            overrides = new List<UpBellReminderOverride>
+            {
+                new UpBellReminderOverride { method = "email", minutes = 24 * 60 },
+                new UpBellReminderOverride { method = "popup", minutes = 10 }
+            };
+        }
+    }
+
+    public class UpBellReminderOverride
+    {
+        public string method { get; set; }
+        public int minutes { get; set; }
+    }
+}
